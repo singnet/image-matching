b7c62b6ecaf452784f879439723fbe416946af23
Anatoly Belikov <awbelikov@gmail.com>
2020-03-29 18:58:24 +0300

    u

---
+++ fem/evaluate_fundus.py
@@ -0,0 +1,181 @@
+import os
+import sys
+import numpy
+from fem.util import caluculate_h, swap_rows, project_points2
+from fem.wrapper import SuperPoint
+from fem import drawing
+import imageio
+import torch
+from superpoint_magicleap.demo_superpoint import PointTracker
+from goodpoint import GoodPoint
+from fem.nonmaximum import MagicNMS
+from fem.bench import get_points_desc, preprocess, draw_matches, replication_ratio, coverage, harmonic
+import cv2
+
+
+
+def in_mask(pts_2, mask):
+    pts2 = pts_2[:2].round().astype(numpy.int16).T
+    return (mask[pts2[:, 1], pts2[:, 0]] > 128).nonzero()[0]
+
+
+GOOD_MATCH_THRESHOLD = 5
+
+
+def main(sp, thresh, nn_thresh=0.8, draw=True):
+
+    matcher = PointTracker(max_length=2, nn_thresh=nn_thresh)
+    fe = None
+    dist_thresh = 0.8
+
+    root_fire = '/mnt/fileserver/shared/datasets/fundus/FIRE/'
+    gt_dir = os.path.join(root_fire, 'Ground Truth')
+    images_dir = os.path.join(root_fire, 'Images')
+    mask_dir = os.path.join(root_fire, 'Masks')
+
+    gt_files = [os.path.join(gt_dir, x) for x in os.listdir(gt_dir)]
+    nCasesTotal = 0
+    mask = imageio.imread(os.path.join(mask_dir, 'mask.png'))
+    mask = imageio.imread(os.path.join(mask_dir, 'feature_mask.png'))
+
+    scale = 0.15
+    mask = cv2.resize(mask, dsize=None, fx=scale, fy=scale)
+    if draw:
+        cv2.imshow('mask', mask)
+    cov_all = []
+    acc_all = []
+    rep_all = []
+    for gt_file in gt_files:
+        name = gt_file.split('control_points_')[1].split('_')[0]
+        img1_path = os.path.join(images_dir, name + '_1.jpg')
+        img2_path = os.path.join(images_dir, name + '_2.jpg')
+        img1 = imageio.imread(img1_path)
+        img2 = imageio.imread(img2_path)
+        img1 = cv2.resize(img1, dsize=None, fx=scale, fy=scale)
+        img2 = cv2.resize(img2, dsize=None, fx=scale, fy=scale)
+
+        img_h, img_w = img1.shape[:2]
+        arr = numpy.loadtxt(gt_file)
+        arr[:, (0, 2)] = arr[:, (0, 2)] * scale
+        arr[:, (1, 3)] = arr[:, (1, 3)] * scale
+        points1 = arr[:, :2]
+        points2 = arr[:, 2:]
+        H, H_inv = caluculate_h(points1, points2)
+        # (x, y) to (row, col)
+        points1 = swap_rows(arr[:, :2].T).T
+        points2 = swap_rows(arr[:, 2:].T).T
+        #in_bounds, points1_projected = project_points2(torch.from_numpy(H), None, torch.from_numpy(points1), img_h, img_w)
+        #drawing.show_points(img1 / 256, points1.astype(numpy.int16), 'points1_img1', 0.5)
+        #drawing.show_points(img2 / 256, points2.astype(numpy.int16), 'points2_img2', 0.5)
+        #drawing.show_points(img2 / 256, points1_projected.cpu().long(), 'points1_img2', 0.5)
+
+        desc_1, pts_1, desc_2, pts_2 = extract(device, fe, img1, img2, mask, sp, thresh)
+        ### debug
+        #img11 = drawing.draw_points(pts_1, img1.copy(), False)
+        #img12 = drawing.draw_points(pts_2, img2.copy(), False)
+        #in_bounds, points1_projected = project_points2(torch.from_numpy(H),
+        #                                               None,
+        #                                               torch.from_numpy(pts_1[:2].T),
+        #                                               img_h,
+        #                                               img_w)
+        #img22 = drawing.draw_points(points1_projected.numpy().T, img2.copy(), False)
+        #cv2.imshow('points11_img1', img11)
+        #cv2.imshow('points22_img2', img12)
+        #cv2.imshow('points11_img2', img22)
+        #cv2.waitKey(0)
+
+        nCasesTotal += 1
+        matches = matcher.nn_match_two_way(desc_1, desc_2, nn_thresh=dist_thresh)
+        nMatches = matches.shape[1]
+
+        pt1 = pts_1[:2, matches[0, :].astype('int32')]
+        pt2 = pts_2[:2, matches[1, :].astype('int32')]
+
+        in_bounds, pt2_p = project_points2(torch.from_numpy(H), None, torch.from_numpy(pt1.T),
+                                           img_h, img_w)
+        nGoodMatches = 0
+        pt2_p = pt2_p.T
+        dx = pt2_p[0, :] - pt2[0, :]
+        dy = pt2_p[1, :] - pt2[1, :]
+        err = numpy.sqrt(dx * dx + dy * dy)
+        rep = 0
+        for i in range(nMatches):
+            if err[i] <= GOOD_MATCH_THRESHOLD:
+                nGoodMatches += 1
+                matches[2, i] = 0
+            else:
+                matches[2, i] = err[i]
+
+        replication = replication_ratio(pt2_p.cpu().numpy(), pts_2, GOOD_MATCH_THRESHOLD)
+        accuracy = float(nGoodMatches) / float(nMatches) if nMatches else 0.0
+        rep_all.append(replication)
+        acc_all.append(accuracy)
+        print('accuracy:', accuracy)
+        print('replication:', replication)
+        if draw:
+            img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2RGB)
+            img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2RGB)
+            from super_debug import draw_desc_interpolate
+            draw_desc_interpolate(img1, img2, swap_rows(pts_1[:2]).T, swap_rows(pts_2[:2]).T, desc_1.T, desc_2.T, 0)
+
+            img_output = draw_matches(matches, pts_1, pts_2, img1, img2)
+            # img_output[:, img_output.shape[1] // 2:] = drawing.draw_points(pt2_p.numpy(),
+            #                                                                img_output[:, img_output.shape[1] // 2:], iscolor=False)
+            cv2.imshow('matches', img_output)
+        coverage1, coverage_mask, intersect = coverage(img1[:,:,-1], mask, matches, pt1)
+        if draw:
+            cv2.imshow('coverage', coverage_mask)
+        cov_all.append(coverage1)
+        if draw:
+            cv2.imshow('itersect', intersect)
+        print('converage:', coverage1)
+        if draw:
+            key = cv2.waitKey(0)
+            if key == ord('q'):
+                break
+        sys.stdout.flush()
+    acc = numpy.mean(acc_all)
+    cov = numpy.mean(cov_all)
+    rep = numpy.mean(rep_all)
+    print('average accuracy:', acc)
+    print('average coverage:', cov)
+    print('average replication:', rep)
+    print('harmonic mean:', harmonic(acc, cov, rep))
+
+
+def extract(device, fe, img1, img2, mask, sp, thresh):
+    img1 = preprocess(img1, cv2.COLOR_BGR2GRAY)
+    pts_1, desc_1 = get_points_desc(fe, sp, device, img1, thresh)
+    in_mask1 = in_mask(pts_1, mask)
+    # apply mask
+    pts_1 = pts_1[:, in_mask1]
+    desc_1 = desc_1[:, in_mask1]
+    img2 = preprocess(img2, cv2.COLOR_BGR2GRAY)
+    pts_2, desc_2 = get_points_desc(fe, sp, device, img2, thresh)
+    in_mask2 = in_mask(pts_2, mask)
+    # apply mask
+    pts_2 = pts_2[:, in_mask2]
+    desc_2 = desc_2[:, in_mask2]
+    return desc_1, pts_1, desc_2, pts_2
+
+
+if __name__ == '__main__':
+    device = 'cuda'
+    weight = "./snapshots/super3400.pt"
+    weight = "./snapshots/super3400.pt"
+    sp_path = '/home/noskill/projects/neuro-fem/fem/superpoint_magicleap/superpoint_v1.pth'
+    nms = MagicNMS(nms_dist=8)
+    sp = SuperPoint(MagicNMS()).to(device).eval()
+    sp.load_state_dict(torch.load(sp_path))
+
+
+    gp = GoodPoint(dustbin=0,
+                   activation=torch.nn.LeakyReLU(),
+                   batchnorm=True,
+                   grid_size=8,
+                   nms=nms).eval().to(device)
+
+    gp.load_state_dict(torch.load(weight, map_location=device)['superpoint'])
+    super_thresh = 0.015
+    thresh = 0.0207190856295525
+    main(gp, thresh=thresh, draw=False, nn_thresh=0.8)
--- fem/evaluation_hpatches_SPv1.py
+++ fem/evaluation_hpatches_SPv1.py
@@ -3,45 +3,65 @@ import numpy
 import util
 import cv2, os
 import sys
-import random
+
 from os import listdir
 from os.path import isfile, join
-from itertools import count
-from superpoint_magicleap.demo_superpoint import SuperPointFrontend
-from superpoint_magicleap.demo_superpoint import PointTracker
+
+
+from superpoint_magicleap.demo_superpoint import PointTracker, SuperPointFrontend
 from goodpoint import GoodPoint
+from fem.wrapper import SuperPoint
 from fem.nonmaximum import MagicNMS
 import torch
-import time
-# import vis_utils
+from fem.bench import get_points_desc, preprocess, draw_matches, replication_ratio, coverage, harmonic
 
-GOOD_MATCH_THRESHOLD = 5 
 
+GOOD_MATCH_THRESHOLD = 5
+fe = None
 dataset_root = '/mnt/fileserver/shared/datasets/SLAM_DATA/hpatches-dataset/hpatches-benchmark/data/hpatches-sequences-release'
 ld = sorted(listdir(dataset_root))
 
+device = 'cuda'
 PATH_WEIGHTS = "./superpoint_magicleap/superpoint_v1.pth"
-fe = None
-#fe = SuperPointFrontend(weights_path=PATH_WEIGHTS,
-#                          nms_dist= 4,
-#                          conf_thresh= 0.025,
-#                          nn_thresh= 0.5,
-#                          cuda=True)
-weight = "./super6300.pt"
+
+weight = "./snapshots/super4200.pt"
+weight = "./super3400.pt"
 nms = MagicNMS(nms_dist=8)
 
-device = 'cuda'
+
+thresh = 0.015
+thresh = 0.0217122295525
+nn_thresh = 0.85
+
+
+
+#fe = SuperPointFrontend(weights_path=PATH_WEIGHTS,
+#                        nms_dist=8,
+#                        conf_thresh=thresh,
+#                        nn_thresh=nn_thresh,
+#                        cuda=True)
+#
+activation = torch.nn.ReLU()
+activation = torch.nn.LeakyReLU()
+
 sp = GoodPoint(dustbin=0,
-               activation=torch.nn.ReLU(),
+               activation=activation,
                batchnorm=True,
                grid_size=8,
                nms=nms).eval().to(device)
+data = torch.load(weight, map_location=device)['superpoint']
 
-sp.load_state_dict(torch.load(weight, map_location=device)['superpoint'])
-thresh=0.056295525
+def strip_module(st):
+    if 'module' in st:
+        return st.split('module.')[1]
+    return st
+
+sp.load_state_dict({strip_module(x): v for (x,v) in data.items()})
 
 
-nn_thresh = 0.8
+if fe is not None:
+    sp = None
+
 matcher = PointTracker(max_length=2, nn_thresh=nn_thresh)
 
 img_output = 0
@@ -51,6 +71,8 @@ nCasesLight = 0
 
 TotalAccuracy = 0
 LightAccuracy = []
+LightCoverage = []
+ViewCoverage = []
 ViewAccuracy = []
 LightReplication = []
 ViewReplication = []
@@ -58,8 +80,11 @@ ViewReplication = []
 break_flag = False
 
 
-imwrite = True
+imwrite = False
+draw = False
 result_dir = './results/'
+
+
 
 for d in range(len(ld)):
     dir = ld[d]
@@ -80,20 +105,12 @@ for d in range(len(ld)):
             break
 
     img_1_src = cv2.imread(img_list[0])
-    img_1 = cv2.cvtColor(img_1_src, cv2.COLOR_RGB2GRAY)
-    timg1 = np.expand_dims(np.expand_dims(img_1.astype('float32'), axis=0), axis=0)
-    if any((1900 < x) for x in timg1.shape):
-        device = 'cpu'
-    else:
-        device = 'cuda'
-
-    if fe is not None:
-        pts_1, desc_1, heatmap_1 = fe.run(img_1.astype('float32') / 255.)
-    else:
-        pts_1, desc_1 = sp.to(device).points_desc(torch.from_numpy(timg1).to(device), threshold=thresh)
-        pts_1 = pts_1.T
-        pts_1 = numpy.concatenate([util.swap_rows(pts_1[:2]), pts_1[2, :][numpy.newaxis,:]])
-        desc_1 = desc_1[0].T.cpu().detach().numpy()
+    img_1 = preprocess(img_1_src)
+    #if any((1900 < x) for x in img_1.shape):
+    #    device = 'cpu'
+    #else:
+    #    device = 'cuda'
+    pts_1, desc_1 = get_points_desc(fe, sp, device, img_1, thresh)
     fReplicatedRatioMean = 0
     for n in range(1, N):
         img_2_src = cv2.imread(img_list[n])
@@ -105,20 +122,9 @@ for d in range(len(ld)):
         dy1 = abs(img_1_src.shape[0] - IMG_SIZE_MAX[0])/2
         dy2 = abs(img_2_src.shape[0] - IMG_SIZE_MAX[0])/2
 
-
-        img_2 = cv2.cvtColor(img_2_src, cv2.COLOR_RGB2GRAY)
-        timg2 = np.expand_dims(np.expand_dims(img_2.astype('float32'), axis=0), axis=0)
-
-        if fe is not None:
-            pts_2, desc_2, heatmap_2 = fe.run(img_2.astype('float32') / 255.)
-        else:
-            pts_2, desc_2 = sp.to(device).points_desc(torch.from_numpy(timg2).to(device), threshold=thresh)
-            pts_2 = pts_2.T
-            desc_2 = desc_2[0].T.cpu().detach().numpy()
-            pts_2 = numpy.concatenate([util.swap_rows(pts_2[:2]), pts_2[2, :][numpy.newaxis,:]])
+        img_2 = preprocess(img_2_src)
+        pts_2, desc_2 = get_points_desc(fe, sp, device, img_2, thresh)
         nCasesTotal += 1
-
-
 
         matches = matcher.nn_match_two_way(desc_1, desc_2, nn_thresh=nn_thresh)
         nMatches = matches.shape[1]
@@ -151,11 +157,17 @@ for d in range(len(ld)):
             if err[i] <= GOOD_MATCH_THRESHOLD:
                 nGoodMatches += 1
                 matches[2, i] = 0
+
+        coverage1, coverage_mask, intersect = coverage(img_1_src[:, :, -1], None, matches, pts_1[:2, matches[0, :].astype('int32')])
+        if draw:
+            cv2.imshow('coverage', coverage_mask)
         accuracy = float(nGoodMatches) / float(nMatches) if nMatches else 0.0
 
         if is_attribs:
+            LightCoverage.append(coverage1)
             LightAccuracy.append(accuracy)
         else:
+            ViewCoverage.append(coverage1)
             ViewAccuracy.append(accuracy)
 
         nCasesTotal += 1
@@ -163,10 +175,8 @@ for d in range(len(ld)):
         # Checking detector repeatability
         pts_1_r=[]
         pts_2_r = pts_2[:2, :]
-        if pts_1.shape[1] > 300:
-            pts_1_r = pts_1[:2, :300]
-        else:
-            pts_1_r = pts_1[:2, :300]
+        pts_1_r = pts_1[:2, :]
+        pts_1_r = pts_1[:2, :]
 
         pts1_r = np.vstack([pts_1_r, np.ones([1, pts_1_r.shape[1]])])
 
@@ -183,51 +193,44 @@ for d in range(len(ld)):
         pt2_p[0, :] = pt2_p[0, :] / pt2_p[2, :]
         pt2_p[1, :] = pt2_p[1, :] / pt2_p[2, :]
 
-        nReplicated = 0
-        for i in range(pts_1_r.shape[1]):
-            match_found = False
-            mindist = 1e8
-            for j in range(pts_2_r.shape[1]):
-                dx = pts_2_r[0, j] - pt2_p[0, i]
-                dy = pts_2_r[1, j] - pt2_p[1, i]
-                err = np.sqrt(dx * dx + dy * dy)
-                if err <= GOOD_MATCH_THRESHOLD:
-                    nReplicated += 1
-                    match_found = True
-                    break
-            if match_found:
-                continue
-
-        fReplicatedRatio = float(nReplicated) / float(pts_1_r.shape[1])
+        # nReplicated = 0
+        # for i in range(pts_1_r.shape[1]):
+        #     match_found = False
+        #     mindist = 1e8
+        #     for j in range(pts_2_r.shape[1]):
+        #         dx = pts_2_r[0, j] - pt2_p[0, i]
+        #         dy = pts_2_r[1, j] - pt2_p[1, i]
+        #         err = np.sqrt(dx * dx + dy * dy)
+        #         if err <= GOOD_MATCH_THRESHOLD:
+        #             nReplicated += 1
+        #             match_found = True
+        #             break
+        #     if match_found:
+        #         continue
+        fReplicatedRatio = replication_ratio(pt2_p, pts_2_r, GOOD_MATCH_THRESHOLD)
+        # fReplicatedRatio = float(nReplicated) / float(pts_1_r.shape[1])
         if is_attribs:
             LightReplication.append(fReplicatedRatio)
         else:
             ViewReplication.append(fReplicatedRatio)
 
-        print("%i\t\tdir: %s\t\t'Replication ratio: %f'\t\tnMatches: %i\tnGoodMatches: %i\t\tMatching accuracy: %f" %
-              (nCasesTotal, dir, fReplicatedRatio, nMatches, nGoodMatches, accuracy))
-
-        img_output = np.zeros(shape=(2 * IMG_SIZE_MAX[0], img_1_src.shape[1] + img_2_src.shape[1], 3), dtype=np.uint8)
-        img_1c = np.repeat(np.expand_dims(img_1.astype('uint8'), axis=2), 3, axis=2)
-        img_2 = np.repeat(np.expand_dims(img_2.astype('uint8'), axis=2), 3, axis=2)
-
-        img_output[IMG_SIZE_MAX[0]:IMG_SIZE_MAX[0]+img_1_src.shape[0], :img_1_src.shape[1], :] = img_1c
-        img_output[IMG_SIZE_MAX[0]:IMG_SIZE_MAX[0]+img_2_src.shape[0], img_1_src.shape[1]:, :] = img_2
-        import drawing
-        drawing.draw_matches(matches, pts_1, pts_2, img_output[IMG_SIZE_MAX[0]:, :, :])
+        print("%i\t\tdir: %s\t\t'Replication ratio: %f'\t\tnMatches: %i\tnGoodMatches: %i\t\tMatching accuracy: %f \t\tCoverage: %f" %
+              (nCasesTotal, dir, fReplicatedRatio, nMatches, nGoodMatches, accuracy, coverage1))
+        if draw or imwrite:
+            img_output = draw_matches(matches, pts_1, pts_2, img_1_src, img_2_src)
         # vis_utils.draw_points(pts_1, img_1c, iscolor=False)
         # vis_utils.draw_points(pts_2, img_2, iscolor=False)
 
         c = 5
-        img_output[:img_1_src.shape[0], :img_1_src.shape[1], :] = img_1c
-        img_output[:img_2_src.shape[0], img_1_src.shape[1]:, :] = img_2
         #cv2.imshow('', img_output[IMG_SIZE_MAX[0]:, :, :])
         if imwrite:
             cv2.imwrite(os.path.join(result_dir, ''.join(img_list[n].split('/')[-2:]) + '.png'), img_output[IMG_SIZE_MAX[0]:, :, :])
-        #c = cv2.waitKey(10)
-        if c == 1048603:
-            break_flag = True
-            break
+        if draw:
+            cv2.imshow('matches', img_output)
+            c = cv2.waitKey(100)
+            if c == ord('q'):
+                break_flag = True
+                break
 
         sys.stdout.flush()
 
@@ -241,9 +244,15 @@ meanViewAccuracy = sum(ViewAccuracy) / float(len(ViewAccuracy))
 meanLightReplication = sum(LightReplication) / float(len(LightReplication))
 meanViewReplication = sum(ViewReplication) / float(len(ViewReplication))
 
+meanLightCoverage = sum(LightCoverage) / float(len(LightCoverage))
+meanViewCoverage = sum(ViewCoverage) / float(len(ViewCoverage))
 
-print("Mean Light Replication: %f\tMean View Replication: %f\tMean Light Accuracy: %f\tMean View Accuracy: %f\t" %
-      (meanLightReplication, meanViewReplication, meanLightAccuracy, meanViewAccuracy) )
+print("Mean Light Replication: %f\tMean View Replication: %f\tMean Light Accuracy: %f\tMean View Accuracy: "
+      "%f\tMean Light Coverage %f\t Mean View Coverage: %f" %
+      (meanLightReplication, meanViewReplication, meanLightAccuracy, meanViewAccuracy, meanLightCoverage, meanViewCoverage) )
 
 
+print('harmonic mean: {0}'.format(harmonic(meanLightReplication, meanViewReplication,
+                                           meanLightAccuracy, meanViewAccuracy,
+                                           meanLightCoverage, meanViewCoverage)))
 print('threshold: {0}'.format(GOOD_MATCH_THRESHOLD))
