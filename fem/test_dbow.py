088a02103c06a30f53162ac831db0d9fd3f82e8f
Anatoly Belikov <awbelikov@gmail.com>
2021-05-11 11:38:53 +0300

    working dbow2 test

--- fem/test_dbow.py
+++ fem/test_dbow.py
@@ -1,4 +1,6 @@
 import torch
+import pickle
+import os
 import numpy as np
 from airsim_dataset import AirsimIntVarDataset
 from torch.utils.data import DataLoader
@@ -45,32 +47,57 @@ def features_to_list(desc):
     return [x[np.newaxis, ...] for x in desc.T]
 
 
-def loop(model, db, loader, thresh, device):
+def norm_average_rank(N, R):
+    N_rel = len(R)
+    return 1 / (N * N_rel) * (sum(R) - N_rel * (N_rel - 1) / 2)
+
+
+def loop(model, db, loader, thresh, device, dataset_name):
     night_features = []
-    for i_batch, sample in enumerate(loader):
-        img_1 = sample['img1'].numpy()
-        img_2 = sample['img2'].numpy()
+    day_features = []
+    day_path = '{0}_day.pkl'.format(dataset_name)
+    night_path = '{0}_night.pkl'.format(dataset_name)
+    if os.path.exists(day_path):
+        print('loading precomputed features')
+        day_features = pickle.load(open(day_path, 'rb'))
+        night_features = pickle.load(open(night_path, 'rb'))
+        for desc1 in day_features:
+            lst1 = features_to_list(desc1)
+            db.add(lst1)
+    else:
+        for i_batch, sample in enumerate(loader):
+            img_1 = sample['img1'].numpy()
+            img_2 = sample['img2'].numpy()
 
-        pts1, desc1 = get_points_desc(device=device,
-                    img=img_1[np.newaxis,...],
-                    thresh=thresh,
-                    fe=None, sp=model)
-        pts2, desc2 = get_points_desc(device=device,
-                    img=img_2[np.newaxis,...],
-                    thresh=thresh,
-                    fe=None, sp=model)
-        night_features.append(desc2)
-        lst1 = features_to_list(desc1)
-        db.add(lst1)
-        if i_batch == 100:
-            break
-
+            pts1, desc1 = get_points_desc(device=device,
+                        img=img_1[np.newaxis,...],
+                        thresh=thresh,
+                        fe=None, sp=model)
+            pts2, desc2 = get_points_desc(device=device,
+                        img=img_2[np.newaxis,...],
+                        thresh=thresh,
+                        fe=None, sp=model)
+            day_features.append(desc1)
+            night_features.append(desc2)
+            lst1 = features_to_list(desc1)
+            db.add(lst1)
+            if not i_batch % 100:
+                print('processing ', i_batch)
+        with open(day_path, 'wb') as f:
+            pickle.dump(day_features, f)
+        with open(night_path, 'wb') as f:
+            pickle.dump(night_features, f)
     import pdb;pdb.set_trace()
+    res = []
     for i, desc2 in enumerate(night_features):
-        query_results = db.query(features_to_list(desc2), 4)
-        for x in query_results:
-            print(x.Id)
-        break
+        query_results = db.query(features_to_list(desc2), len(day_features) + 1)
+        ids = [x.Id for x in query_results]
+        expected  = [x for x in range(i - 3, i + 3) if x >= 0]
+        indexes = [ids.index(x) + 1 for x in expected if x in ids]
+        rank = norm_average_rank(len(day_features), indexes)
+        print(rank)
+        res.append(rank)
+    print(np.mean(res))
 
 
 def main():
@@ -85,7 +112,7 @@ def main():
     village_loader, fantasy_loader = get_datasets()
 
     model = get_model(device)
-    loop(model, db, village_loader, thresh, device)
+    loop(model, db, village_loader, thresh, device, 'village')
 
 
 if __name__ == '__main__':
