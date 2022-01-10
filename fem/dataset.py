05c8d2b1015ba0f5067d8dac5d14040af34fcb71
Anatoly Belikov <awbelikov@gmail.com>
2022-01-10 15:48:45 +0300

    use imageio for image loading

--- fem/dataset.py
+++ fem/dataset.py
@@ -6,6 +6,7 @@ from operator import itemgetter
 import torch
 from torch.utils import data
 import imageio
+import skimage.color
 import numpy
 import cv2
 
@@ -81,9 +82,13 @@ class SynteticShapes(data.Dataset):
         if self.color.value == ColorMode.GREY.value:
             pilmode = 'L'
         if self.color.value == ColorMode.GREY.value:
-            img = cv2.imread(path, 0)
+            img = imageio.imread(path)
+            # make sure it's rgb
+            if len(img.shape) == 3:
+                assert img.shape[2] == 3
+                img = skimage.color.rgb2gray(img)
         else:
-            img = cv2.imread(path, 1)
+            img = imageio.imread(path)
         if len(img.shape) == 2:
             img = img.reshape((*img.shape, 1))
         return numpy.asarray(img)
@@ -189,3 +194,15 @@ class ImageSelfLearnDataset(ImageDirectoryDataset):
         images = torch.stack([torch.from_numpy(x.astype(numpy.float32)) for x in transformed[0]])
         H = torch.stack([torch.from_numpy(x) for x in transformed[1]])
         return images, H
+
+
+class MinecraftDataset(ImageDirectoryDataset):
+    def _load_dataset(self, subset=None):
+        result = []
+        p = self.path
+        for img_name in os.listdir(p):
+            if img_name.startswith('seg'):
+                continue
+            result.append(os.path.join(p, img_name))
+        return result
+
